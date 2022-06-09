64ba17a7ca9e1e77d9d84d06105e93a08afe9092
mehdi_aq <douahmehdi.p@gmail.com>
2022-06-09 05:03:58 +0200

    Initial commit

---
+++ precache-manifest.86ba66da061db9954bab9d8b052a6cb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc38e72c21e4997dd1b355e0f4a2231",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aab6b2cdb413b5e68aa",
+    "url": "/static/css/main.15465b7f.chunk.css"
+  },
+  {
+    "revision": "ddab59933b63efe3f753",
+    "url": "/static/js/2.3a155715.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3a155715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aab6b2cdb413b5e68aa",
+    "url": "/static/js/main.09239ba9.chunk.js"
+  },
+  {
+    "revision": "868b87a273a143f9f087",
+    "url": "/static/js/runtime-main.77e1f623.js"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/static/media/digital-7.regular.1e670d88.ttf"
+  }
+]);
